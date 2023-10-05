9909402c8d8308c4e4c8e6ea7b0aeac3925ea310
elput0amo <gmiradej@myuax.com>
2023-10-05 13:26:07 +0200

    si

---
+++ gravedad.py
@@ -0,0 +1,24 @@
+gravedades = {
+   "Mercurio" : -3.7,
+   "Venus" : -8.87,
+    "Tierra": -9.81,
+    "Marte" : -3.71,
+    "Jupiter": -28.79,
+    "Saturno" :-10.44,
+    "Urano" : -8.87,
+    
+
+}
+#esta es la base de datos de los planetas y sus gravedades
+
+planeta = input ("En que planeta estas: ")
+gravedad = gravedades [planeta]
+print (f" La gravedad de tu planeta {gravedad} ")
+altura1 = float (input ("Altura inicial (m): " ))
+time = float(input ("En que segundo lo quieres (en el instante inicial es 0): "))
+#estos son los valores que va a introducir el usuario
+
+altura_variable = altura1+gravedad*time
+#esta va a ser la altura que se va a imprimir 
+
+print (f"Tu altura en el segundo {time:.2f} es de {altura_variable:.2f} m/s")
---
+++ mi amigo.py
@@ -0,0 +1,11 @@
+nombre = input("Hola manin, ¿cual es tu nombre?: ")
+ano = int(input(f"Hola {nombre}, tienes nombre de puto pringado, ¿de que año eres?: "))
+gustos = input(f"Buah eres viejo, tienes {2023-ano}años, dime que te gusta hacer a ver si tus gustos son igual de lamentables que tu edad: ")
+print (f"Vale la gente que le gusta {gustos} son unos gilipollas, no quiero hablar contigo, adios")
+madre = int( input ("Es broma tio, en verdad me caes bien y no eres tan viejo, seguro que tu madre es muy joven, ¿cuantos años tiene?:" ))
+if madre >= 69 :
+    print ("Vale, queria ver si era una milf, pero es demasiado vieja para mi, te vere dentro de poco en su funeral")
+else :
+    numero = input("Oye, dame su numero para preguntarle lo maravilloso/a que eres")
+    a= input (f"¿El numero es {numero}?")
+    print ("Perfecto, voy a ser tu nuevo padrastro, te veo en navidad hijo")
